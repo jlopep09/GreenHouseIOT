e0bfaa4b73ed20a407494a8bba5269ea38e2346a
jlopep09 <jlopep09@estudiantes.unileon.es>
2024-12-10 12:28:53 +0100

    updated terms text

--- COMPONENTS/s02_GreenHouse_DataMining/app/app/views/footer.py
+++ COMPONENTS/s02_GreenHouse_DataMining/app/app/views/footer.py
@@ -1,7 +1,28 @@
 import reflex as rx
+class FooterState(rx.State):
+    
+    home_url:str = "/"
+    about_url:str = "/about"
+    github_url:str = "https://github.com/jlopep09/GreenHouseIOT"
+    contact_url:str = "/contact"
+    policy_url:str = "/policy"
+    terms_url:str = "/terms"
 
-def footer_item(text: str, href: str) -> rx.Component:
-    return rx.link(rx.text(text, size="3"), href=href)
+    @rx.event
+    def go_to_home(self):
+        return rx.redirect(self.home_url, external=False)
+    @rx.event
+    def go_to_about(self):
+        return rx.redirect(self.about_url, external=False)
+    @rx.event
+    def go_to_github(self):
+        return rx.redirect(self.github_url, external=True)
+    @rx.event
+    def go_to_contact(self):
+        return rx.redirect(self.contact_url, external=False)
+
+def footer_item(text: str, href: str, external:bool = False) -> rx.Component:
+    return rx.link(rx.text(text, size="3"), href=href, is_external=external)
 
 
 def footer_items_1() -> rx.Component:
@@ -9,8 +30,8 @@ def footer_items_1() -> rx.Component:
         rx.heading(
             "PRODUCTS", size="4", weight="bold", as_="h3"
         ),
-        footer_item("Greenhouse IOT", "/#"),
-        footer_item("Repository", "/#"),
+        footer_item("Greenhouse IOT", FooterState.about_url),
+        footer_item("Repository", FooterState.github_url, external = True),
         spacing="4",
         text_align=["center", "center", "start"],
         flex_direction="column",
@@ -22,8 +43,8 @@ def footer_items_2() -> rx.Component:
         rx.heading(
             "ABOUT US", size="4", weight="bold", as_="h3"
         ),
-        footer_item("Contact Us", "/#"),
-        footer_item("Our Team", "/#"),
+        footer_item("Contact Us", FooterState.contact_url),
+        footer_item("Our Team", FooterState.about_url),
         spacing="4",
         text_align=["center", "center", "start"],
         flex_direction="column",
@@ -35,24 +56,24 @@ def footer_items_3() -> rx.Component:
         rx.heading(
             "RESOURCES", size="4", weight="bold", as_="h3"
         ),
-        footer_item("Privacy Policy", "/#"),
-        footer_item("Terms of Service", "/#"),
+        footer_item("Privacy Policy", FooterState.policy_url),
+        footer_item("Terms of Service", FooterState.terms_url),
         spacing="4",
         text_align=["center", "center", "start"],
         flex_direction="column",
     )
 
 
-def social_link(icon: str, href: str) -> rx.Component:
-    return rx.link(rx.icon(icon), href=href)
+def social_link(icon: str, href: str, external:bool = False) -> rx.Component:
+    return rx.link(rx.icon(icon), href=href, is_external=external)
 
 
 def socials() -> rx.Component:
     return rx.flex(
-        social_link("instagram", "/#"),
-        social_link("twitter", "/#"),
-        social_link("github", "/#"),
-        social_link("mail", "/#"),
+        social_link("instagram", FooterState.contact_url),
+        social_link("twitter", FooterState.contact_url),
+        social_link("github", FooterState.github_url, external=True),
+        social_link("mail", "mailto:jlopep09@estudiantes.unileon.es"),
         spacing="3",
         justify_content=["center", "center", "end"],
         width="100%",
