00613ba3b255d2c747ae956448adaf2641ff25e3
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-04-09 14:30:18 +0200

    Ajustado api bbdd para usar hypercorn y se ha eliminado de forma temporal el uso de kafka

--- COMPONENTS/s01_GreenHouse_DataCore/app/main.py
+++ COMPONENTS/s01_GreenHouse_DataCore/app/main.py
@@ -1,8 +1,11 @@
-import asyncio
+"""import asyncio"""
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+"""
 from routers.db_router import router as db_router
+"""
 from fastapi.middleware.cors import CORSMiddleware
-import kafka_module.consumer as kf
+"""import kafka_module.consumer as kf"""
 
 
 
@@ -13,15 +16,18 @@ origins = [
 
 app = FastAPI()
 
-
+"""
 app.include_router(db_router)
-
-@app.on_event("startup")
+"""
+@app.get("/")
+async def get_root():
+    return PlainTextResponse("Hello, its working")
+"""@app.on_event("startup")
 async def startup_event():
     print(f"Conectando a Kafka en kafka:9092...")
     asyncio.create_task(kf.consume_messages())
 
-
+"""
 
 app.add_middleware(
     CORSMiddleware,
