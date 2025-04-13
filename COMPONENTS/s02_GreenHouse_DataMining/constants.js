f8f7b23bc9731c4be95f6e75cb5d061fe9e301b4
Jose López <jlopep09@estudiantes.unileon.es>
2025-04-13 22:20:54 +0200

    actualizadas constantes

--- COMPONENTS/s02_GreenHouse_DataMining/constants.js
+++ COMPONENTS/s02_GreenHouse_DataMining/constants.js
@@ -1,3 +1,2 @@
-
-export const appName = "GreenhouseIOT"
-export const ddbbApiIp = "https://fastapiddbb-production.up.railway.app"
+export const appName =  "GreenhouseIOT";
+export const ddbbApiIp = import.meta.env.VITE_DDBB_API_IP || "https://localhost:8002";
