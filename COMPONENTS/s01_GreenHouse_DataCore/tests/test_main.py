5591b5526c58c05ee6fcc931e31fe9c9a9c6b573
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-07-14 15:29:49 +0200

    Cambios en las dependencias

--- COMPONENTS/s01_GreenHouse_DataCore/tests/test_main.py
+++ COMPONENTS/s01_GreenHouse_DataCore/tests/test_main.py
@@ -1,10 +1,11 @@
-from starlette.testclient import TestClient
-from app.main import app  # Ajusta esta ruta si es diferente
+import pytest
+from httpx import AsyncClient
+from app.main import app
 
-client = TestClient(app)
-
-def test_health_check():
-    response = client.get("/health")
-    assert response.status_code == 200
-    assert response.text == "Hello, its working"
-    assert response.headers["content-type"] == "text/plain; charset=utf-8"
+@pytest.mark.anyio
+async def test_health_check():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/health")
+        assert response.status_code == 200
+        assert response.text == "Hello, its working"
+        assert response.headers["content-type"] == "text/plain; charset=utf-8"
