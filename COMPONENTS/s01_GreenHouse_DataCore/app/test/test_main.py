411afa01a6aebfb926de9040b52467858b89732b
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-07-14 14:58:38 +0200

    incluida dependencia para coverage y pytest

---
+++ COMPONENTS/s01_GreenHouse_DataCore/app/test/test_main.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from app.main import app  # Ajusta esta ruta si es diferente
+
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.text == "Hello, its working"
+    assert response.headers["content-type"] == "text/plain; charset=utf-8"
